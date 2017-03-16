038f29ed8b11df7dc6b2fd829b8b55c4ca39cc5b
sambapython@gmail.com <sambapython@gmail.com>
2017-03-16 08:07:09 +0530

    2017-03-16

---
+++ pe1.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+
+def fun():
+    print a
+    print b
+s1 = "rrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+s = "wweferwerwerwerwerwerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+s3 = s + s1
+s4 = "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss\
+sssssssssasasdasdasdasdasdasdasdasdasdsadasdasdsadasdas"
+print s4
